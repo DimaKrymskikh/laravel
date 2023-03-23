afedc120b2b89bb58aa158439f986dfd49a813be
Дима Крымских <krymskikh@gmail.com>
2023-03-23 20:20:03 +0700

    Написал команду statistics:filmsAndUsers и тест для неё

---
+++ app/Console/Commands/Dvd/FilmsStatisticsAvailableToUsers.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands\Dvd;
+
+use App\Models\Person\User;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class FilmsStatisticsAvailableToUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'statistics:filmsAndUsers {isFile?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Статистика фильмов в наличие у пользователей';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->info('Старт.');
+        
+        $films = $this->getStatistics();
+        
+        if ($this->argument('isFile')) {
+            $this->writeFile($films, "commannds\Dvd\FilmsStatisticsAvailableToUsers_" . date('d-m-Y') . ".txt");
+        } else {
+            $this->writeConsole($films);
+        }
+        
+        $this->info('Команда выполнена.');
+    }
+    
+    private function getStatistics(): Collection
+    {
+        return User::leftJoin('person.users_films', 'person.users_films.user_id', '=', 'person.users.id')
+                ->select('person.users.id', 'person.users.login', DB::raw('count(person.users_films.film_id) as n'))
+                ->groupBy('person.users.id')
+                ->orderBy('n', 'desc')
+                ->orderBy('person.users.login')
+                ->get();
+    }
+    
+    private function writeConsole(Collection $films): void
+    {
+        $this->line("$this->description\n");
+        
+        foreach ($films as $film) {
+            $this->line("$film->login [$film->id] имеет в своём списке $film->n фильмов.");
+        }
+    }
+    
+    private function writeFile(Collection $films, string $file): void
+    {
+        Storage::disk('local')->put($file, "$this->description\n");
+        
+        foreach ($films as $film) {
+            Storage::append($file, "$film->login [$film->id] имеет в своём списке $film->n фильмов.");
+        }
+    }
+}
---
+++ tests/Feature/Commands/Dvd/FilmsStatisticsAvailableToUsersTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Commands\Dvd;
+
+use App\Models\User;
+use App\Models\Person\UserFilm;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FilmsStatisticsAvailableToUsersTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_statistics_films_and_users_in_console(): void
+    {
+        list($userTestLogin, $userTestLogin2) = $this->getData();
+        
+        $this
+            ->artisan('statistics:filmsAndUsers')
+            ->expectsOutput("BaseTestLogin [1] имеет в своём списке 3 фильмов.")
+            ->expectsOutput("$userTestLogin->login [$userTestLogin->id] имеет в своём списке 2 фильмов.")
+            ->expectsOutput("UserTestLogin2 [$userTestLogin2->id] имеет в своём списке 0 фильмов.")
+            ->assertExitCode(0);
+    }
+    
+    
+    public function test_statistics_films_and_users_in_file(): void
+    {
+        $this->getData();
+        
+        $this
+            ->artisan('statistics:filmsAndUsers true')
+            ->assertExitCode(0);
+    }
+    private function getData(): array
+    {
+        $userTestLogin = User::factory()->create();
+        
+        $userTestLogin2 = User::factory()
+            ->state(['login' => 'UserTestLogin2'])
+            ->create();
+
+        UserFilm::factory()
+                ->count(5)
+                ->state(new Sequence(
+                    ['film_id' => 7],
+                    ['film_id' => 15],
+                    ['film_id' => 21],
+                    [
+                        'user_id' => $userTestLogin->id,
+                        'film_id' => 7
+                    ],
+                    [
+                        'user_id' => $userTestLogin->id,
+                        'film_id' => 555
+                    ]
+                ))
+                ->create();
+        
+        return [$userTestLogin, $userTestLogin2];
+    }
+}
