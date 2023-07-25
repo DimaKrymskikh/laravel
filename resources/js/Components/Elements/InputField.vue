914a715457a9483c655167e6b0e8578bc1ef972f
Дима Крымских <krymskikh@gmail.com>
2023-07-25 20:42:59 +0700

    Поправил InputField

--- resources/js/Components/Elements/InputField.vue
+++ resources/js/Components/Elements/InputField.vue
@@ -1,20 +1,28 @@
 <script setup>
+import Spinner from '@/components/Svg/Spinner.vue';
+
 defineProps({
     titleText: String,
     type: String,
     errorsMessage: String,
-    modelValue: String
+    modelValue: String,
+    isRequest: Boolean
 });
+
 defineEmits(['update:modelValue']);
 </script>
 
 <template>
-    <label>{{ titleText }}
-        <input
-            :type="type"
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
-        />
-    </label>
-    <div v-if="errorsMessage" class="error">{{ errorsMessage }}</div>
+    <div class="mb-2">
+        <label>
+            <span class="text-orange-700">{{ titleText }}</span>
+            <Spinner hSpinner="h-8" class="flex justify-center" v-if="isRequest"/>
+            <input v-else
+                :type="type"
+                :value="modelValue"
+                @input="$emit('update:modelValue', $event.target.value)"
+            />
+        </label>
+        <div v-if="errorsMessage" class="error">{{ errorsMessage }}</div>
+    </div>
 </template>
