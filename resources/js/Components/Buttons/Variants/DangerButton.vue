a7b3cfb78c451a030872b438344e9324df9f45a7
Дима Крымских <krymskikh@gmail.com>
2023-07-25 19:59:17 +0700

    Создал кнопки DangerButton и PrimaryButton

---
+++ resources/js/Components/Buttons/Variants/DangerButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    buttonText: String,
+    handler: Function
+});
+</script>
+
+<template>
+    <button
+        class="px-4 py-2 bg-red-500 text-white hover:bg-red-600 rounded-lg"
+        @click="handler"
+    >
+        {{ buttonText }}
+    </button>
+</template>
+
---
+++ resources/js/Components/Buttons/Variants/PrimaryButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    buttonText: String,
+    handler: Function
+});
+</script>
+
+<template>
+    <button
+        class="px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded-lg"
+        @click="handler"
+    >
+        {{ buttonText }}
+    </button>
+</template>
